0500efb0c71949655543edf7e11759c9f7afef4b
chylex <contact@chylex.com>
2022-09-11 14:46:16 +0200

    Add log watcher

---
+++ src/log_watcher.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::{Error, ErrorKind};
+use std::path::PathBuf;
+
+use linemux::MuxedLines;
+use tokio::sync::mpsc::UnboundedSender;
+
+use crate::log_file_pattern::LogFilePath;
+
+pub async fn read_logs_task(log_files: Vec<LogFilePath>, shutdown_send: UnboundedSender<()>) {
+	if let Err(error) = read_logs(log_files).await {
+		println!("[LogWatcher] Error reading logs: {}", error);
+		shutdown_send.send(()).unwrap();
+	}
+}
+
+async fn read_logs(log_files: Vec<LogFilePath>) -> io::Result<()> {
+	let mut file_reader = MuxedLines::new()?;
+	let mut label_lookup: HashMap<PathBuf, &String> = HashMap::new();
+	
+	for log_file in &log_files {
+		let lookup_key = file_reader.add_file(&log_file.path).await?;
+		label_lookup.insert(lookup_key, &log_file.label);
+	}
+	
+	if log_files.is_empty() {
+		println!("[LogWatcher] No log files provided.");
+		return Err(Error::from(ErrorKind::Unsupported));
+	}
+	
+	println!("[LogWatcher] Watching {} log file(s).", log_files.len());
+	
+	loop {
+		let event_result = file_reader.next_line().await?;
+		if let Some(event) = event_result {
+			match label_lookup.get(event.source()) {
+				Some(label) => {
+					println!("[LogWatcher] Received line from \"{}\": {}", label, event.line());
+				}
+				None => {
+					println!("[LogWatcher] Received line from unknown file: {}", event.source().display());
+				}
+			}
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use tokio::signal;
+use tokio::sync::mpsc;
+
 use crate::log_file_pattern::parse_log_file_pattern_from_env;
+use crate::log_watcher::read_logs_task;
 
 mod log_file_pattern;
+mod log_watcher;
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+	println!("Initializing exporter...");
+	
 	let log_file_pattern = match parse_log_file_pattern_from_env() {
 		Ok(pattern) => pattern,
 		Err(error) => {
@@ -19,7 +27,27 @@ fn main() {
 		}
 	};
 	
-	for log_file in log_files {
+	if log_files.is_empty() {
+		println!("Found no matching log files.");
+		return;
+	}
+	
+	for log_file in &log_files {
 		println!("Found log file: {} (label \"{}\")", log_file.path.display(), log_file.label);
+	}
+	
+	let (shutdown_send, mut shutdown_recv) = mpsc::unbounded_channel();
+	tokio::spawn(read_logs_task(log_files, shutdown_send.clone()));
+	
+	drop(shutdown_send);
+	
+	tokio::select! {
+		_ = signal::ctrl_c() => {
+			println!("Received CTRL-C, shutting down...")
+		}
+		
+		_ = shutdown_recv.recv() => {
+			println!("Shutting down...");
+		}
 	}
 }
